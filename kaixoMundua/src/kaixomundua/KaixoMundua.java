a336df9b5e374ce64a4c734ca548424715fb4fef
Sir James <Sir James@SirJames-PC>
2013-10-26 16:12:32 +0200

    Lehenengo proba.

---
+++ kaixoMundua/src/kaixomundua/KaixoMundua.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kaixomundua;
+
+/**
+ *
+ * @author Sir James
+ */
+public class KaixoMundua {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Kaixo mundua!");
+    }
+}
